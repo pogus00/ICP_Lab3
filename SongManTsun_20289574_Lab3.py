c6f04b459765cf7331551bf9665f5bb7f30955da
pogus00 <songmantsun2006@gmail.com>
2024-10-07 18:04:18 +0800

    update

---
+++ SongManTsun_20289574_Lab3.py
@@ -0,0 +1,15 @@
+"""CCIT4020 Introduction to Computer Programming
+HKU SPACE Community College
+laboratory 2"""
+
+#Section 1: Interfacing
+print("CCIT4020 Laboratory 3: Section 1")
+
+string = input("Input a string, followed by <ENTER> key:")
+print(f"Sting <{string}> is entered!")
+integer = int(input("Input a integer, followed by <ENTER> key:"))
+print(f"Integer <{integer}> is entered!")
+float = float(input("Input a float, followed by <ENTER> key:"))
+print(f"Float <{float}> is entered!")
+print("\nCompleted by Song Man Tsun ...CCIT4020 ICP...")
+
